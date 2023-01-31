0e04a199af90a3242b858c42b33abd2d5bf833b2
jihnk <jhyunk218@gmail.com>
2023-02-01 00:45:49 +0900

    fix: 모달 열린 상태에서  스크롤 방지 추가

--- src/hooks/useModal.ts
+++ src/hooks/useModal.ts
@@ -1,8 +1,15 @@
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 type UseModalType = [boolean, () => void];
 
 const useModal = (initialValue: boolean): UseModalType => {
 	const [isOpenModal, setIsOpenModal] = useState(initialValue);
+	useEffect(() => {
+		if (isOpenModal) {
+			document.body.style.overflow = "hidden";
+		} else {
+			document.body.style.overflow = "";
+		}
+	}, [isOpenModal]);
 
 	const handleModal = useCallback(
 		() => setIsOpenModal(!isOpenModal),
