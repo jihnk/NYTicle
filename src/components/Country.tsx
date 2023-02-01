be5dedb09242122db17f37aaaa03023cffe228c7
jihnk <jhyunk218@gmail.com>
2023-02-01 11:17:37 +0900

    fix: 국가 목록 id추가

--- src/components/Country.tsx
+++ src/components/Country.tsx
@@ -1,10 +1,10 @@
 interface CountryItemProps {
-	text: string;
+	name: string;
 }
-const CountryItem = ({ text }: CountryItemProps) => {
+const CountryItem = ({ name }: CountryItemProps) => {
 	return (
 		<button className="flex flex-none justify-center items-center w-fit border border-gray rounded-[16px] gap-1 px-2 py-0.5">
-			<p className="text-md text-unactive">{text}</p>
+			<p className="text-md text-unactive">{name}</p>
 		</button>
 	);
 };
